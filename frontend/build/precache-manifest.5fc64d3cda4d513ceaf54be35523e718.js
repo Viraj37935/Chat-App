674acc38e5a9bfaa9e74057f0e23fb75b77a9c6c
V37935 <virajdimble6@gmail.com>
2024-03-12 22:50:43 +0530

    done

---
+++ frontend/build/precache-manifest.5fc64d3cda4d513ceaf54be35523e718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e36e9e0451af9dfaf407191f890859b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4982a4d61f925a3f1d4",
+    "url": "/static/css/main.01055aca.chunk.css"
+  },
+  {
+    "revision": "dffdc622e7180cd193cf",
+    "url": "/static/js/2.d97a7846.chunk.js"
+  },
+  {
+    "revision": "89d1e5374f5de0e26f70",
+    "url": "/static/js/3.3afed3ca.chunk.js"
+  },
+  {
+    "revision": "f4982a4d61f925a3f1d4",
+    "url": "/static/js/main.12657f28.chunk.js"
+  },
+  {
+    "revision": "ee947ca885e43acb10fc",
+    "url": "/static/js/runtime~main.76392e3d.js"
+  },
+  {
+    "revision": "0c0a181f54a5754fe278d6a043efc329",
+    "url": "/static/media/background.0c0a181f.png"
+  }
+]);
